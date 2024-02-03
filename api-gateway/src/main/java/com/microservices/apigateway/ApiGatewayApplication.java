4da6ded836fb95d10750523727d223e787da7e52
rahulkokje <rahul.kokje@vivy.com>
2024-02-03 12:21:43 +0100

    add API gateway

---
+++ api-gateway/src/main/java/com/microservices/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,37 @@
+package com.microservices.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+
+		return builder.routes()
+				.route(p -> p
+						.path("/product-service/**")
+						.filters(f -> f.rewritePath("/product-service/(?<remaining>.*)", "/${remaining}"))
+						.uri("lb://product-service")
+				)
+				.route(p -> p
+						.path("/order-service/**")
+						.filters(f -> f.rewritePath("/order-service/(?<remaining>.*)", "/${remaining}"))
+						.uri("lb://order-service")
+				)
+				.route(p -> p
+						.path("/inventory-service/**")
+						.filters(f -> f.rewritePath("/inventory-service/(?<remaining>.*)", "/${remaining}"))
+						.uri("lb://inventory-service")
+				)
+				.build();
+	}
+}
